690633aa35643237d3600996b22fd23d1d8dfb71
George Deng <georgedengit@gmail.com>
2015-09-14 20:38:16 -0700

    moved some files to dropbox instead of version control.

--- env.sh
+++ env.sh
@@ -7,21 +7,9 @@ export PROJECT_HOME=$HOME/Projects
 
 # PATH
 export PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
-if [[ "$OSTYPE" == "linux-gnu" ]]; then
-    export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$HOME/.rbenv/bin:$PATH"
-fi
 
 # Owner
 export USER_NAME="YOUR NAME"
-
-# Node.js
-export NVM_DIR=~/.nvm
-if [[ "$OSTYPE" == "darwin"* ]]; then
-    source $(brew --prefix nvm)/nvm.sh
-fi
-
-# Ruby
-eval "$(rbenv init -)"
 
 # FileSearch
 function f() { find . -iname "*$1*" ${@:2} }
@@ -35,6 +23,4 @@ alias ffs='sudo $(fc -ln -1)'
 if [[ "$OSTYPE" == "darwin"* ]]; then
     alias bu='brew update && brew upgrade --all && brew cleanup && brew doctor'
     alias cppcompile='c++ -std=c++11 -stdlib=libc++'
-    alias dbstart='launchctl load ~/Library/LaunchAgents/homebrew.mxcl.mysql.plist && launchctl load ~/Library/LaunchAgents/homebrew.mxcl.mongodb.plist'
-    alias dbstop='launchctl unload ~/Library/LaunchAgents/homebrew.mxcl.mysql.plist && launchctl unload ~/Library/LaunchAgents/homebrew.mxcl.mongodb.plist'
 fi
