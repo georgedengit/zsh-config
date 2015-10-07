2a242a219b8873e9d38a536e1782726c929e399d
George Deng <georgedengit@gmail.com>
2015-10-07 15:20:03 -0700

    Mac OS X specific branch

--- env.sh
+++ env.sh
@@ -7,8 +7,5 @@ export PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
 alias ffs='sudo $(fc -ln -1)'
 alias rmhist='rm $HISTFILE'
 
-# OS-Specific Aliases
-if [[ "$OSTYPE" == "darwin"* ]]; then
-    alias bu='brew update && brew upgrade --all && brew cleanup && brew doctor'
-    alias cppcompile='c++ -std=c++11 -stdlib=libc++'
-fi
+alias bu='brew update && brew upgrade --all && brew cleanup && brew doctor'
+alias cppcompile='c++ -std=c++11 -stdlib=libc++'
