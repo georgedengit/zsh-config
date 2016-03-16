8e809283f872879935f2c0267400da07878d1716
George Deng <georgedengit@gmail.com>
2016-03-16 15:59:32 -0700

    pyenv and virtualenvwrapper

--- .zshrc
+++ .zshrc
@@ -1,26 +1,14 @@
-# Environment variables
+# Zsh Variables
 export ZSH=$HOME/.oh-my-zsh
 export ZSH_CONFIG=$HOME/src/config
-export JAVA_HOME=`/usr/libexec/java_home -v 1.8`
-export MAVEN_HOME=/usr/local/apache-maven-3.3.9
-export MAVEN_OPTS=-Djava.net.preferIPv4Stack=true
-export WORKON_HOME=~/Envs
 
 # Theme
 ZSH_THEME="pygmalion"
 
 # Plugins
-plugins=(git npm colored-man colorize github vagrant virtualenv pip python brew osx zsh-syntax-highlighting)
+plugins=(git npm colored-man colorize github vagrant pip python brew osx zsh-syntax-highlighting)
 
 # Sources
 source $ZSH/oh-my-zsh.sh
 source $ZSH_CONFIG/env.sh
 source $ZSH_CONFIG/npm-completion.sh
-source /usr/local/bin/virtualenvwrapper.sh
-
-# Aliases
-alias configzsh="vim $ZSH_CONFIG"
-alias sourcezsh="source ~/.zshrc"
-
-# fpath
-fpath=(/usr/local/share/zsh-completions $fpath)
--- env.sh
+++ env.sh
@@ -1,14 +1,31 @@
- #!/bin/zsh
+#!/bin/zsh
 
 # PATH
 export PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
-export PATH="./node_modules/.bin:$PATH"
-export PATH="$PATH:$JAVA_HOME/bin"
-export PATH="$PATH:$MAVEN_HOME/bin"
 
 # Aliases
+alias bu='brew update && brew upgrade --all && brew cleanup && brew doctor'
 alias ffs='sudo $(fc -ln -1)'
 alias rmhist='rm $HISTFILE'
-
-alias bu='brew update && brew upgrade --all && brew cleanup && brew doctor'
 alias cppcompile='c++ -std=c++11 -stdlib=libc++'
+
+# fpath
+fpath=(/usr/local/share/zsh-completions $fpath)
+
+# java
+export JAVA_HOME=`/usr/libexec/java_home -v 1.8`
+export MAVEN_HOME=/usr/local/apache-maven-3.3.9
+export MAVEN_OPTS=-Djava.net.preferIPv4Stack=true
+export PATH="$JAVA_HOME/bin:$MAVEN_HOME/bin:$PATH"
+
+# node
+export PATH="./node_modules/.bin:$PATH"
+
+# pyenv
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
+
+# virtualenvwrapper
+export VIRTUALENVWRAPPER_PYTHON=`which python`
+source /usr/local/bin/virtualenvwrapper.sh
