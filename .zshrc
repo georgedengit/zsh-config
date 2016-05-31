cce65e349b2d062e9e1d20db2ac70c29138040df
George Deng <georgedengit@gmail.com>
2016-05-30 18:27:27 -0700

    changed zshrc and env

--- .zshrc
+++ .zshrc
@@ -8,6 +8,18 @@ ZSH_THEME="pygmalion"
 # Plugins
 plugins=(git npm colored-man colorize github vagrant pip python brew osx zsh-syntax-highlighting)
 
+# PATH
+export PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
+
+# fpath
+fpath=(/usr/local/share/zsh-completions $fpath)
+
+# Aliases
+alias bu='brew update && brew upgrade --all && brew cleanup && brew doctor'
+alias ffs='sudo $(fc -ln -1)'
+alias rmhist='rm $HISTFILE'
+alias cppcompile='c++ -std=c++11 -stdlib=libc++'
+
 # Sources
 source $ZSH/oh-my-zsh.sh
 source $ZSH_CONFIG/env.sh
--- env.sh
+++ env.sh
@@ -1,31 +1,18 @@
 #!/bin/zsh
 
-# PATH
-export PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
-
-# Aliases
-alias bu='brew update && brew upgrade --all && brew cleanup && brew doctor'
-alias ffs='sudo $(fc -ln -1)'
-alias rmhist='rm $HISTFILE'
-alias cppcompile='c++ -std=c++11 -stdlib=libc++'
-
-# fpath
-fpath=(/usr/local/share/zsh-completions $fpath)
-
-# java
+# Java
 export JAVA_HOME=`/usr/libexec/java_home -v 1.8`
 export MAVEN_HOME=/usr/local/apache-maven-3.3.9
 export MAVEN_OPTS=-Djava.net.preferIPv4Stack=true
 export PATH="$JAVA_HOME/bin:$MAVEN_HOME/bin:$PATH"
 
-# node
+# Node
 export PATH="./node_modules/.bin:$PATH"
 
-# pyenv
-export PYENV_ROOT="$HOME/.pyenv"
-export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init -)"
-
-# virtualenvwrapper
-export VIRTUALENVWRAPPER_PYTHON=`which python`
-source /usr/local/bin/virtualenvwrapper.sh
+# Python
+export PYENV_ROOT=/usr/local/var/pyenv
+if which pyenv > /dev/null; then
+    eval "$(pyenv init -)"
+    export PYENV_VIRTUALENVWRAPPER_PREFER_PYVENV="true"
+    pyenv virtualenvwrapper
+fi
