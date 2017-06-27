f78d13b4f0b67975ba1d01826391eaaab9c65193
George Deng <georgedengit@gmail.com>
2017-06-27 14:17:05 -0700

    updated env example with nodejs and pyenv-virtualenv

--- env-example.sh
+++ env-example.sh
@@ -7,8 +7,14 @@ prompt pure
 # Java
 eval "$(jenv init -)"
 
+# NodeJS
+export NVM_DIR="$HOME/.nvm"
+. "/usr/local/opt/nvm/nvm.sh"
+
 # Python
+export PYENV_ROOT="$HOME/.pyenv"
 eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
 
 # Ruby
 eval "$(rbenv init -)"
