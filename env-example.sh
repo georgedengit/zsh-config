a6978ad4bc47f151dbf36032a960b38ef6d18407
George Deng <georgedengit@gmail.com>
2017-07-03 21:38:12 -0700

    remove node from env example (prefer tj/n)

--- env-example.sh
+++ env-example.sh
@@ -7,10 +7,6 @@ prompt pure
 # Java
 eval "$(jenv init -)"
 
-# NodeJS
-export NVM_DIR="$HOME/.nvm"
-. "/usr/local/opt/nvm/nvm.sh"
-
 # Python
 export PYENV_ROOT="$HOME/.pyenv"
 eval "$(pyenv init -)"
