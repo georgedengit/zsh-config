da2265254380c0e5597a60fe166db955b8529a5d
George Deng <gdeng@rivian.com>
2020-10-26 15:23:42 -0700

    optimize mac zsh performance

--- .zshrc
+++ .zshrc
@@ -6,17 +6,10 @@ export ZSH_CONFIG=$HOME/src/zsh-config
 ZSH_THEME=""
 
 # Plugins
-plugins=(brew docker git node npm yarn pyenv pip python jenv)
+plugins=(evalcache brew git npm yarn docker docker-compose)
 
 # PATH
 export PATH="/usr/local/bin:/usr/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin"
-
-# Aliases
-alias brew="env PATH=${PATH//$(pyenv root)\/shims:/} brew"
-alias bu='brew update && brew upgrade && brew cleanup && brew doctor'
-alias configzsh='code $ZSH_CONFIG'
-alias ffs='sudo $(fc -ln -1)'
-alias rmhist='rm $HISTFILE'
 
 # Sources
 source $ZSH/oh-my-zsh.sh
--- env-example.sh
+++ env-example.sh
@@ -5,14 +5,25 @@ autoload -U promptinit && promptinit
 prompt pure
 
 # Java
-eval "$(jenv init -)"
+if command -v jenv 1>/dev/null 2>&1; then
+    export PATH="$HOME/.jenv/bin:$PATH"
+    _evalcache jenv init -
+fi
 
 # Python
 if command -v pyenv 1>/dev/null 2>&1; then
     export PYENV_ROOT="$HOME/.pyenv"
-    eval "$(pyenv init -)"
+    _evalcache pyenv init -
 fi
 
+# direnv
+if command -v direnv 1>/dev/null 2>&1; then
+    _evalcache direnv hook zsh
+fi
 
-# Ruby
-eval "$(rbenv init -)"
+# Aliases
+alias brew='env PATH=${PATH//$(pyenv root)\/shims:/} brew'
+alias bu='brew update && brew upgrade && brew cleanup && brew doctor'
+alias configzsh='code $ZSH_CONFIG'
+alias ffs='sudo $(fc -ln -1)'
+alias rmhist='rm $HISTFILE'
